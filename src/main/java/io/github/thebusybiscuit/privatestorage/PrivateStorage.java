592fc4ca8d443097ddab4efddeeb97ed7b312e62
LobbyTech <1187755698@qq.com>
2021-03-13 19:12:38 +0800

    Update PrivateStorage.java

--- src/main/java/io/github/thebusybiscuit/privatestorage/PrivateStorage.java
+++ src/main/java/io/github/thebusybiscuit/privatestorage/PrivateStorage.java
@@ -1,6 +1,5 @@
 package io.github.thebusybiscuit.privatestorage;
 
-import org.bstats.bukkit.Metrics;
 import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,28 +7,13 @@ import io.github.thebusybiscuit.privatestorage.storage.PrivateChests;
 import io.github.thebusybiscuit.privatestorage.storage.PublicChests;
 import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import me.mrCookieSlime.Slimefun.Objects.Category;
-import me.mrCookieSlime.Slimefun.cscorelib2.config.Config;
 import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
 import me.mrCookieSlime.Slimefun.cscorelib2.skull.SkullItem;
-import me.mrCookieSlime.Slimefun.cscorelib2.updater.GitHubBuildsUpdater;
-import me.mrCookieSlime.Slimefun.cscorelib2.updater.Updater;
 
 public class PrivateStorage extends JavaPlugin implements SlimefunAddon {
 
     @Override
     public void onEnable() {
-        Config cfg = new Config(this);
-
-        // Setting up bStats
-        new Metrics(this, 4912);
-
-        if (getDescription().getVersion().startsWith("DEV - ")) {
-            Updater updater = new GitHubBuildsUpdater(this, getFile(), "TheBusyBiscuit/PrivateStorage/master");
-            // Only run the Updater if it has not been disabled
-            if (cfg.getBoolean("options.auto-update")) {
-                updater.start();
-            }
-        }
 
         Category category = new Category(new NamespacedKey(this, "private_storage"), new CustomItem(SkullItem.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZThlNTU0NGFmN2Y1NDg5Y2MyNzQ5MWNhNjhmYTkyMzg0YjhlYTVjZjIwYjVjODE5OGFkYjdiZmQxMmJjMmJjMiJ9fX0="), "&7箱子工艺", "", "&a> 点击打开"));
 
